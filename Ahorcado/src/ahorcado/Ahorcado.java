8fa48abed350895ae4a06c023052897bf87e3963
alumno <alumno@aula1-02>
2019-10-29 14:40:00 +0100

    Inicio del método preguntarLetra

--- Ahorcado/src/ahorcado/Ahorcado.java
+++ Ahorcado/src/ahorcado/Ahorcado.java
@@ -1,10 +1,129 @@
 package ahorcado;
 
+import java.util.Scanner;
+
 public class Ahorcado {
 
+	
+	//Método que comprueba que la partida se ha terminado
+	//O pierdo las vidas o gano la partida
+	public static boolean partidaTerminada() {
+		boolean terminado=false;
+		
+		return terminado;
+	}
+	
+	public static void inicializarVectores(String vHuecos[], String vSolucion[], String palabra){
+		for (int i=0; i<vHuecos.length; i++) {
+			vHuecos[i] = "_";
+			vSolucion[i] = palabra.substring(i, i+1);
+		}
+		
+	}
+	
+	public static void pintaMuñeco(String vFallos[], String vHuecos[]) {
+		int fallos = 0;
+		
+		for (int i = 0; i<vFallos.length; i++) {
+			if (vFallos[i] != null) {
+				fallos = fallos + 1;
+			}
+		}
+		switch (fallos) {
+		case 1:
+			System.out.println("_________________");
+			break;
+		case 2:
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|_________________");
+			break;
+		case 3:
+			System.out.println("__________");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|_________________");
+			break;
+		case 4:
+			System.out.println("__________");
+			System.out.println("|         |");
+			System.out.println("|         |");
+			System.out.println("|        <0>");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|_________________");
+			break;
+		case 5:
+			System.out.println("__________");
+			System.out.println("|         |");
+			System.out.println("|         |");
+			System.out.println("|        <0>");
+			System.out.println("|         |");
+			System.out.println("|         |");
+			System.out.println("|");
+			System.out.println("|");
+			System.out.println("|_________________");
+			break;
+		case 6:
+			System.out.println("__________");
+			System.out.println("|         |");
+			System.out.println("|         |");
+			System.out.println("|        <0>");
+			System.out.println("|        /|\\");
+			System.out.println("|         |");
+			System.out.println("|        / \\");
+			System.out.println("|");
+			System.out.println("|_________________");
+			break;
+		}
+		
+		System.out.println();
+		System.out.println();
+		for (int i = 0; i<vHuecos.length; i++) {
+			System.out.print(vHuecos[i] + " ");
+		}
+		
+		System.out.println();
+		System.out.println("Fallos:");
+		for (int i = 0; i<vFallos.length; i++) {
+			if (vFallos[i] != null) {
+				System.out.print(vFallos[i] + " ");
+			}
+		}
+	}
+	//Pregunta la letra y actualiza los vectores
+	//Si se encuentra actualiza vHuecos
+	//Si fallo actualiza vFallos
+	public static void preguntarLetra(String vFallos[], String vHuecos[], String vSolucion[]) {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String palabraSolucion = "HOLA";
+		String vFallos[], vSolucion[], vHuecos[];
+		
+		vFallos = new String[6];
+		vHuecos = new String[palabraSolucion.length()];
+		vSolucion = new String[palabraSolucion.length()];
+		
+		inicializarVectores(vHuecos, vSolucion, palabraSolucion);
+		
+		do {
+			pintaMuñeco(vFallos, vHuecos);
+			preguntarLetra(vFallos, vHuecos, vSolucion);
+		}while(partidaTerminada());
 	}
 
 }
